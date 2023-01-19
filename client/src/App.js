ae2d3fd190ca39a3be69b3add3d459185e591d96
Nikhil Sharma <sharmanikhil6922@gmail.com>
2023-01-20 01:09:56 +0530

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const addItem = async (e) => {
     e.preventDefault();
     try{
-      const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+      const res = await axios.post('http://52.91.230.251:5500/api/item', {item: itemText})
       setListItems(prev => [...prev, res.data]);
       setItemText('');
     }catch(err){
@@ -24,7 +24,7 @@ function App() {
   useEffect(()=>{
     const getItemsList = async () => {
       try{
-        const res = await axios.get('http://localhost:5500/api/items')
+        const res = await axios.get('http://52.91.230.251:5500/api/items')
         setListItems(res.data);
         console.log('render')
       }catch(err){
@@ -37,7 +37,7 @@ function App() {
   // Delete item when click on delete
   const deleteItem = async (id) => {
     try{
-      const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
+      const res = await axios.delete(`http://52.91.230.251:5500/api/item/${id}`)
       const newListItems = listItems.filter(item=> item._id !== id);
       setListItems(newListItems);
     }catch(err){
@@ -49,7 +49,7 @@ function App() {
   const updateItem = async (e) => {
     e.preventDefault()
     try{
-      const res = await axios.put(`http://localhost:5500/api/item/${isUpdating}`, {item: updateItemText})
+      const res = await axios.put(`http://52.91.230.251:5500/api/item/${isUpdating}`, {item: updateItemText})
       console.log(res.data)
       const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
       const updatedItem = listItems[updatedItemIndex].item = updateItemText;
